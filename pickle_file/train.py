0a2ed79a0ef77be0ee78b8b0fec3e7776bac9df4
siddharth <siddharth654choudhary@gmail.com>
2020-06-16 17:41:24 +0530

    added train file

---
+++ pickle_file/train.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import pickle
+df = pd.read_csv('data\m14_merged.csv', index_col=0)
+
+feats =   ['BVP_mean', 'BVP_std', 'EDA_phasic_mean', 'EDA_phasic_min', 'EDA_smna_min', 
+           'EDA_tonic_mean', 'Resp_mean', 'Resp_std', 'TEMP_mean', 'TEMP_std', 'TEMP_slope', 'BVP_peak_freq', 'age', 'height', 'weight', 'label']  
+
+df2 = df[feats]
+X = df2.drop('label', axis=1).values
+y = df2['label'].values
+df2.label.value_counts()
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)  
+
+
+
+#DECISION TREE
+from sklearn import tree
+model = tree.DecisionTreeClassifier()
+model.fit(X_train,y_train)
+model.score(X_test,y_test)
+filename = 'decisionTree_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+#BAGGING MODEL -  RANDOM FOREST ALGORITHM 
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators=40)#doubt 
+model.fit(X_train,y_train)
+model.score(X_test,y_test)
+filename = 'gradBoost_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+
+#GRADBOOST ALGORITHM 
+from sklearn.ensemble import GradientBoostingClassifier
+model = GradientBoostingClassifier(random_state=0) 
+model.fit(X_train,y_train)
+model.score(X_test,y_test)
+filename = 'randomForest_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+
+
+
+# {
+#     "BVP_mean": -0.181673262,
+#     "BVP_std": 107.648359,
+#     "EDA_phasic_mean": 1.824289,
+#     "EDA_phasic_min": 0.367977083,
+#     "EDA_smna_min": 5.22965803e-08,
+#     "EDA_tonic_mean": 1.23216412,
+#     "Resp_mean": 0.148183977,
+#     "Resp_std": 2.93561681,
+#     "TEMP_mean": 35.8170909,
+#     "TEMP_std": 0.0126739141,
+#     "TEMP_slope": -0.000169059802,
+#     "BVP_peak_freq": 0.13566987,
+#     "age": 27.0,
+#     "height": 175.0,
+#     "weight": 80.0
+# }
